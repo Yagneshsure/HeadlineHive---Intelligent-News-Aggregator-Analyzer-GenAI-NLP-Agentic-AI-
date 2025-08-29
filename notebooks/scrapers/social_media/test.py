24163f68c438a0389f60c299ebd5c4832ebf6696
Yagnesh Sure <yagneshsure10@gmail.com>
2025-08-29 15:14:13 +0530

    new change

--- notebooks/scrapers/social_media/test.py
+++ notebooks/scrapers/social_media/test.py
@@ -1,3 +1,12 @@
-import snscrape.modules.twitter as sntwitter
-import pandas as pd
-from datetime import datetime, timedelta
+from gnews import GNews
+
+google_news = GNews(language='en', country='IN')
+
+# Search for "Artificial Intelligence"
+news = google_news.get_news('Artificial Intelligence')
+
+for item in news[:5]:
+    print(item['title'])
+    print(item['url'])
+    print(item['published date'])
+    print("-" * 80)
